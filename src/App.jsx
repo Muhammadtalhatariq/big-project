645df0dcc0181fdaac288d543f4318b3d3606d0c
muhammadtalha <mtalhatariqx@gmail.com>
2024-03-05 10:23:45 -0800

    setup the project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css"
+
+ const App = () => {
+  console.log(import.meta.env.VITE_APPWRITTE_URL);
+  return (
+    <div className="nav">this is a app</div>
+    
+  )
+}
+
+export default App;
