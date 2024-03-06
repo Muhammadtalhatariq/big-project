d5945ddfc1f8efd535e86de44b24beb071fc28ae
muhammadtalha <mtalhatariqx@gmail.com>
2024-03-06 05:05:01 -0800

    store ready

---
+++ src/Store/authSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  status: false,
+  userDate: null,
+};
+
+const authSlice = createSlice({
+  name: auth,
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.status = true;
+      state.userDate = action.payload.userDate;
+    },
+    logout: (state) => {
+      state.status = false;
+      state.userDate = null;
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+
+export default authSlice.reducer;
