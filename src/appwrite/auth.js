9baafc1025d8653502530ae564aa71b9eb44c527
muhammadtalha <mtalhatariqx@gmail.com>
2024-03-06 00:54:28 -0800

    Create backend setup

---
+++ src/appwrite/auth.js
@@ -0,0 +1,59 @@
+import conf from "../../conf/conf.js";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthServices {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccont({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        // call another function
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCuttentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite serive :: getCuttentUser:: error ", error);
+    }
+    return null;
+  }
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Appwrite serive :: logout :: error ", error);
+    }
+  }
+}
+const authServices = new AuthServices();
+
+export default authServices;
