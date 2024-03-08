6f592d0a732cf89fe92a1609fd90e40866f486c1
muhammadtalha <mtalhatariqx@gmail.com>
2024-03-08 03:11:54 -0800

    post form create

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,16 @@
 import Header from "./Header/Header";
-import Footer from  './Footer/Footer';
+import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import Logoutbtn from "./Header/Logoutbtn";
 import Container from "./container/Container";
+import RTE from "./RTE";
+import Select from "./Select";
 export {
-    Header,
-    Footer,
-   Logo,
-   Logoutbtn,
-   Container,
-
-}
+     Header,
+    Footer, 
+    Logo, 
+    Logoutbtn,
+    Container,
+     RTE,
+     Select ,
+        };
