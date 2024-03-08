685950f0058aaa786fc5c0f3c5bf69d42114c88c
muhammadtalha <mtalhatariqx@gmail.com>
2024-03-08 03:24:24 -0800

    update index file

--- src/components/index.js
+++ src/components/index.js
@@ -5,12 +5,28 @@ import Logoutbtn from "./Header/Logoutbtn";
 import Container from "./container/Container";
 import RTE from "./RTE";
 import Select from "./Select";
+import Input from "./Input";
+import Login from "./Login";
+import Sighup from "./Signup";
+import Button from "./Button";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import PostForm from "./Post-Form/PostForm";
+
+
 export {
-     Header,
-    Footer, 
-    Logo, 
-    Logoutbtn,
-    Container,
-     RTE,
-     Select ,
-        };
+  Header,
+  Footer,
+  Logo,
+  Logoutbtn,
+  Container,
+  RTE,
+  Select,
+  Input,
+  Login,
+  Sighup,
+  Button,
+  PostCard,
+  AuthLayout,
+  PostForm,
+};
